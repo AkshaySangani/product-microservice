4723204948d93980be853b0aeec9ff9cd6972c41
Akshay <akshay10sangani@gmail.com>
2025-05-18 01:48:34 +0530

    bug fix

--- src/routes/collaboration/vendor.ts
+++ src/routes/collaboration/vendor.ts
@@ -3,8 +3,9 @@ import { VendorAuthMiddleware } from '../../middleware/vendorAuth.middleware';
 import { getCreatorWiseProductList, sendCollaborationRequestToCreator } from '../../controller/collaboration/vendor/vendorCollaboration.controller';
 const router = Router();
 
-router.get('/creator/product/list', VendorAuthMiddleware, getCreatorWiseProductList); // get product list when vendor send collb request
+router.get('/creator/product/list/:creatorId', VendorAuthMiddleware, getCreatorWiseProductList); // get product list when vendor send collb request
 
 router.post('/request-creator', VendorAuthMiddleware, sendCollaborationRequestToCreator); // send bid to creator
 
+// router.delete('/cancel-request/:collaborationId', VendorAuthMiddleware, cancelCollaborationRequest);
 export { router as vendorCollaborationRouter };
