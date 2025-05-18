f69b010854989522606140dd5346be322ea220bd
Akshay <akshay10sangani@gmail.com>
2025-05-18 18:11:50 +0530

    collab list bug fix

--- src/controller/collaboration/creator/creatorCollaboration.controller.ts
+++ src/controller/collaboration/creator/creatorCollaboration.controller.ts
@@ -124,7 +124,7 @@ const sendCollaborationRequestToVendor = async (
           path: "productId",
           populate: [{ path: "category", model: "Category" }],
         })
-        .populate("creatorId")
+        .populate("vendorId")
         .skip((Number(page) - 1) * Number(limit))
         .limit(Number(limit))
         .sort({ createdAt: -1 });
